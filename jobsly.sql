002bf1f8c567fb24f268f00bb45065a97a6952fa
rhsantiago <reggie1@gmail.com>
2016-12-02 17:32:54 +0800

    54. update DB

--- jobsly.sql
+++ jobsly.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2016 at 11:58 AM
+-- Generation Time: Dec 02, 2016 at 10:31 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -19,6 +19,34 @@ SET time_zone = "+00:00";
 --
 -- Database: `jobsly`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `educationandtraining`
+--
+
+CREATE TABLE `educationandtraining` (
+  `id` int(10) NOT NULL,
+  `userid` int(10) NOT NULL,
+  `hsschool` varchar(50) DEFAULT NULL,
+  `hsadd` varchar(50) DEFAULT NULL,
+  `hsgraddate` date DEFAULT '0000-00-00',
+  `hsawards` text,
+  `coluni` varchar(50) DEFAULT NULL,
+  `coladd` varchar(50) DEFAULT NULL,
+  `colgpa` varchar(10) DEFAULT NULL,
+  `colgraddate` date DEFAULT '0000-00-00',
+  `colmajor` varchar(30) DEFAULT NULL,
+  `colawards` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `educationandtraining`
+--
+
+INSERT INTO `educationandtraining` (`id`, `userid`, `hsschool`, `hsadd`, `hsgraddate`, `hsawards`, `coluni`, `coladd`, `colgpa`, `colgraddate`, `colmajor`, `colawards`) VALUES
+(7, 1, 'schhol', '87 Spain st.', '2016-11-01', '<p>Hello Summernote</p>', NULL, NULL, NULL, '0000-00-00', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -50,8 +78,7 @@ CREATE TABLE `personalinformation` (
 --
 
 INSERT INTO `personalinformation` (`id`, `userid`, `lname`, `fname`, `mname`, `street`, `city`, `province`, `country`, `mnumber`, `myemail`, `landline`, `age`, `birthday`, `gender`, `nationality`) VALUES
-(4, 5, 'Santiago', 'Regidor', '', '', '', NULL, '', '', '', NULL, 0, '0000-00-00', '', ''),
-(36, 5, 'Santiago', 'Regidor', '', '87 Spain St. Better Living Subd.', 'Paranaque', 'Metro Manila', 'Philippines', '9175010845', 'reggie1@gmail.com', ' 639175010845', 1, '2016-11-02', 'female', 'dsd');
+(7, 1, 'Santiago', 'Regidor', 'Hernandez', '87 Spain st., Better Living Subd', 'Paranaque', 'Metro Manila', 'Philippines', '  (23)3343', 'reggie1@gmail.com', ' 6391770018451', 39, '2016-11-09', 'female', 'Philippines');
 
 -- --------------------------------------------------------
 
@@ -75,7 +102,7 @@ CREATE TABLE `useraccounts` (
 --
 
 INSERT INTO `useraccounts` (`id`, `email`, `password`, `usertype`, `companyname`, `verifyhash`, `signupdate`, `isverified`) VALUES
-(1, 'reg@jobsly.net', 'regsam', 2, NULL, '11111', '2016-11-07', 0),
+(1, 'reg@jobsly.net', 'regsam', 2, NULL, '11111', '2016-11-07', 1),
 (27, 'reggie1@gmail.com', 'r', 2, 'undefined', '222', '2016-11-12', 0);
 
 -- --------------------------------------------------------
@@ -94,12 +121,28 @@ CREATE TABLE `workexperience` (
   `industry` varchar(40) DEFAULT NULL,
   `plevel` varchar(30) NOT NULL,
   `enddate` date DEFAULT '0000-00-00',
-  `currentemployer` varchar(5) DEFAULT NULL
+  `currentemployer` varchar(5) DEFAULT NULL,
+  `jobdescription` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `workexperience`
+--
+
+INSERT INTO `workexperience` (`id`, `userid`, `company`, `position`, `startdate`, `msalary`, `industry`, `plevel`, `enddate`, `currentemployer`, `jobdescription`) VALUES
+(7, 1, 'CHAMP Cargosystems Inc.', 'Senior Software Engineer', '2016-11-07', 85000, 'Airline Cargo', 'Senior', '2016-11-24', 'on', '<p>Accomplishments:</p><ul><li>Architecture Committee reviewer</li><li>Developer for Handling and Airline modules</li><li>Trains new joiners</li><li>Refactoring group</li></ul>'),
+(15, 1, 'Rappler', 'posiiton', '2016-12-09', 3333, 'industry', 'Manager', '2016-11-16', 'on', '<p>Hello Summernote</p>'),
+(16, 1, '', '', '0000-00-00', 0, '', '', '0000-00-00', 'on', '<p>Hello Summernote</p>');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `educationandtraining`
+--
+ALTER TABLE `educationandtraining`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `personalinformation`
@@ -124,10 +167,15 @@ ALTER TABLE `workexperience`
 --
 
 --
+-- AUTO_INCREMENT for table `educationandtraining`
+--
+ALTER TABLE `educationandtraining`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
 -- AUTO_INCREMENT for table `personalinformation`
 --
 ALTER TABLE `personalinformation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `useraccounts`
 --
@@ -137,7 +185,7 @@ ALTER TABLE `useraccounts`
 -- AUTO_INCREMENT for table `workexperience`
 --
 ALTER TABLE `workexperience`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
