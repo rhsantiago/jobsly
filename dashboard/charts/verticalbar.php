27226386d263ad470e7d5a34cd091302a7ca0e3d
rhsantiago <reggie1@gmail.com>
2017-05-31 18:54:08 +0800

    207. add chart folder

--- dashboard/charts/verticalbar.php
+++ dashboard/charts/verticalbar.php
@@ -1,6 +1,6 @@
 <section class="blog-post">
     <div class="panel panel-default">                                    
-         <div class="panel-body jobad-bottomborder">
+         <div class="panel-body">
                 <div><h4 class="text-info h4weight">Monthly Views</h4></div>
           </div>
  <style>
@@ -21,30 +21,35 @@
 <script src="//d3js.org/d3.v3.min.js" charset="utf-8"></script>    
         
 <script>
-    var data = [10,25,38,52,100];
-    var width = 500;
-    var height = 40;
+    var data = [30,25,38,52,110];
+    var margin = {top: 20, right: 20, bottom: 40, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
     
     var heightScale = d3.scale.linear()
+                    .domain([0,d3.max(data)])
+                    .range([height,0]);
+    
+    var dataHeightScale = d3.scale.linear()
                     .domain([0,d3.max(data)])
                     .range([0,height]);
     
     var newScaledData = [];
     for (var i = 0; i < data.length; i++) {
-      newScaledData[i] = heightScale(data[i]);
+      newScaledData[i] = dataHeightScale(data[i]);
     }
     
     var yaxis = d3.svg.axis()
                 .orient("left")
-                .ticks(3)                
+                .ticks(5)              
                 .scale(heightScale);
-    
+
     var canvas = d3.select("body")
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height)
                 .append("g")
-                .attr("transform", "translate(40,0)");
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
     var bars = canvas.selectAll("rect")
                 .data(newScaledData)
@@ -52,15 +57,18 @@
                     .append("rect")
                     .attr("height", function(d){return d;})
                     .attr("width", 20)
+                    .attr("fill", "teal")                    
                     .attr("x", function(d,i){ return i * 22;})
                     .attr("y", function(d) {
                                     return height - d;  //Height minus data value
                                 });
-    /*
+   
     canvas.append("g")
-            .attr("transform", "translate(0,0)")
+            .attr("transform", "translate(-2,0)")      
+            .attr("font-size",8)
+              .style({ 'stroke': 'black', 'fill': 'none', 'stroke-width': '1px'})
             .call(yaxis);
-    */    
+       
 </script>
      </div>
 </section>
